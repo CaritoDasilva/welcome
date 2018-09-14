7cfd98da2301c43c7983bf8dc694e0a53e0b492f
CaritoDasilva <caritodasilva@gmail.com>
2018-09-14 16:49:47 -0300

    ultimos avances

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 
+//importa dependencias de Material.angular.io
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    BrowserAnimationsModule
   ],
   imports: [
     BrowserModule
